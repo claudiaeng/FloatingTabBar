881816dad675cf0e786c8c54a6ccc7ed1af72f0c
Claudia Eng <claudiaeng@live.com>
2020-05-16 22:04:44 -0400

    Update Colors.swift

--- Sources/FloatingTabBar/Colors.swift
+++ Sources/FloatingTabBar/Colors.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-extension Color {
-    static var random: Color {
+public extension Color {
+    public static var random: Color {
         return Color(red: .random(in: 0...1),
                      green: .random(in: 0...1),
                      blue: .random(in: 0...1))
